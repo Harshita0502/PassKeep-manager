bd5a5e23f0f94e5459daf8b26186ca505defa33e
HarshitaIndurkar <114684181+Harshita0502@users.noreply.github.com>
2023-11-30 21:02:15 +0530

    Update PassKeep.cpp

--- PassKeep.cpp
+++ PassKeep.cpp
@@ -1,125 +1,87 @@
 #include <iostream>
 #include <fstream>
-#include <cctype>
 #include <string>
-#include <vector>
+#include <cstdlib>
 #include <filesystem>
 
-using namespace std;
 namespace fs = std::filesystem;
 
-struct User {
-    string username;
-    string birthdate;
-    string password;
-};
+bool isStrongPassword(const std::string& password) {
+    // Check length
+    if (password.length() < 8) {
+        return false;
+    }
 
-bool containsSubstring(const string& str, const string& substr) {
-    return str.find(substr) != string::npos;
-}
-
-void printStrength(const string& password, const string& username, const string& birthdate) {
-    int n = password.length();
-
-    // Checking lower alphabet in string
-    bool hasLower = false, hasUpper = false;
-    bool hasDigit = false;
-    int specialCount = 0; // Count of special characters
-    string normalChars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890 ";
-    string specialChars = "@#!$%^&*()_+=-~`.<>,;:'\"/?|\\][}{";
-
-    for (int i = 0; i < n; i++) {
-        if (islower(password[i]))
+    // Check for lowercase, uppercase, digit, and special character
+    bool hasLower = false, hasUpper = false, hasDigit = false, hasSpecial = false;
+    for (char ch : password) {
+        if (islower(ch)) {
             hasLower = true;
-        if (isupper(password[i]))
+        } else if (isupper(ch)) {
             hasUpper = true;
-        if (isdigit(password[i]))
+        } else if (isdigit(ch)) {
             hasDigit = true;
-
-        // Check for special characters and count them
-        if (specialChars.find(password[i]) != string::npos) {
-            specialCount++;
+        } else if (ispunct(ch) || ch == ' ') {
+            hasSpecial = true;
         }
     }
 
-    // Check if the password contains the username or birthdate
-    bool containsUsername = containsSubstring(password, username);
-    bool containsBirthdate = containsSubstring(password, birthdate);
-
-    // Strength of password
-    cout << "\nPassKeep Strength: ";
-    if (hasLower && hasUpper && hasDigit &&
-        specialCount >= 2 && (n >= 8) && !containsUsername && !containsBirthdate)
-        cout << "Strong\n";
-    else if ((hasLower || hasUpper || hasDigit) &&
-            specialCount >= 2 && (n >= 6) && !containsUsername && !containsBirthdate)
-        cout << "Moderate\n";
-    else
-        cout << "Weak\n";
-
-    // Check if the password contains the username or birthdate and provide a warning
-    if (containsUsername || containsBirthdate) {
-        cout << "Warning: Avoid using your username or birthdate in the password.\n";
-    }
+    // Check if all criteria are met
+    return (hasLower && hasUpper && hasDigit && hasSpecial);
 }
 
-void saveToFile(const string& filename, const string& username, const string& password) {
-    ofstream file(filename);
+void savePassword(const std::string& siteName, const std::string& username, const std::string& password) {
+    std::string folderName = "PassKeep";
+    if (!fs::exists(folderName)) {
+        fs::create_directory(folderName);
+    }
 
-    if (file.is_open()) {
-        file << "Username: " << username << endl;
-        file << "Password: " << password << endl;
-        cout << "Credentials saved to file: " << filename << endl;
-        file.close();
+    std::string fileName;
+    std::cout << "Enter the file name (without extension) to save the password: ";
+    std::cin >> fileName;
+
+    std::ofstream outputFile(folderName + "/" + fileName + ".txt");
+    if (outputFile.is_open()) {
+        outputFile << "SiteName: " << siteName << std::endl;
+        outputFile << "Username: " << username << std::endl;
+        outputFile << "Password: " << password << std::endl;
+        outputFile.close();
+        std::cout << "Password saved successfully in " << folderName << "/" << fileName << ".txt\n";
     } else {
-        cerr << "Unable to open file " << filename << " for writing." << endl;
+        std::cout << "Unable to save the password.\n";
     }
 }
 
 int main() {
-    cout << "PassKeep Manager and Strength Checker\n";
-    cout << "-------------------------------------\n";
+    std::string password;
+    bool strongPassword = false;
 
-    vector<User> users; // Store user information
+    while (!strongPassword) {
+        std::cout << "Enter your password: ";
+        std::cin >> password;
 
-    char choice;
-    cout << "Do you want to use PassKeep Manager? (y/n): ";
-    cin >> choice;
+        if (isStrongPassword(password)) {
+            std::cout << "Strong password!\n";
 
-    if (tolower(choice) == 'y') {
-        string folderName = "PassKeep";
-        fs::create_directory(folderName);
+            char saveChoice;
+            std::cout << "Do you want to save this password? (y/n): ";
+            std::cin >> saveChoice;
 
-        string filename;
-        cout << "Enter the name of the file to be saved (without extension): ";
-        cin >> filename;
-        filename = folderName + "/" + filename + ".txt";
+            if (tolower(saveChoice) == 'y') {
+                std::string siteName, username;
+                std::cout << "Enter the site you want to use this password for: ";
+                std::cin.ignore(); // Clearing input buffer
+                std::getline(std::cin, siteName);
+                std::cout << "Enter your username on the site: ";
+                std::getline(std::cin, username);
 
-        while (fs::exists(filename)) {
-            cout << "File already exists. Enter a different name: ";
-            cin >> filename;
-            filename = folderName + "/" + filename + ".txt";
+                savePassword(siteName, username, password);
+            }
+
+            strongPassword = true;
+        } else {
+            std::cout << "Weak password! Password should contain at least 8 characters, including lowercase, uppercase, digit, and special character.\n";
         }
-
-        cout << "Enter your username: ";
-        string username;
-        cin >> username;
-
-        cout << "Enter your birthdate (YYYYMMDD): ";
-        string birthdate;
-        cin >> birthdate;
-
-        cout << "Enter your password: ";
-        string password;
-        cin >> password;
-
-        saveToFile(filename, username, password);
-    }
-
-    // Password strength check for each user
-    for (const auto& user : users) {
-        cout << "\nPassKeep Strength for User: " << user.username << endl;
-        printStrength(user.password, user.username, user.birthdate);
     }
 
     return 0;
